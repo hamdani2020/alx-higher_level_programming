c4f4c3da073637282db4237747eb9d74624561f3
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-06-06 22:16:32 -0700

    first commit

---
+++ 0x01-python-if_else_loops_functions/3-print_alphabt.py
@@ -0,0 +1,6 @@
+#!/usr/bin/python3
+for letter in range (97, 123):
+    if chr(letter) != 'q' and chr(letter) != 'e':
+        print("{}".format(chr(letter)), end="")
+
+#Author: Alhassan Hamdani Gandi
