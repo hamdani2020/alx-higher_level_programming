238408c0c6e3c355cf186174b4adcc717e4f97fe
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-06-08 13:46:49 -0700

    hot fix

--- 0x02-python-import_modules/1-calculation.py
+++ 0x02-python-import_modules/1-calculation.py
@@ -1,7 +1,9 @@
 #!/usr/bin/python3
 
 if __name__ == "__main__":
-     """Print the sum, substration, multiple and quotient of 10 and 5."""
+     """Print the addition, substration, multiplication and division of 
+     10 and 5.
+     """
     from calculator_1 import add, sub, mul, div
 
     a = 10
