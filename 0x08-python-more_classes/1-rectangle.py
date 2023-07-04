04e27a012e7db6bffaed33a57db84cb128028d82
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-07-04 06:09:37 -0700

    real definition of rectangle

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -1,41 +1,40 @@
 #!/usr/bin/python3
 """
-Define a class rectangle
+Defines a class Rectangle
 """
 
+
 class Rectangle:
-    """Representation of a Rectangle."""
+    """Representation of a rectangle"""
     def __init__(self, width=0, height=0):
-        """Initialzes the rectangle"""
+        """Initializes the rectangle"""
         self.height = height
         self.width = width
 
     @property
     def width(self):
-        """Getter for the private instance"""
+        """getter for the private instance attribute width"""
         return self.__width
 
     @width.setter
     def width(self, value):
-        """Setter for the private instance"""
+        """setter for the private instance attribute width"""
         if type(value) is not int:
             raise TypeError("width must be an integer")
-        if type(value) < 0:
+        if value < 0:
             raise ValueError("width must be >= 0")
         self.__width = value
 
     @property
     def height(self):
-        """Getter for the private instance"""
+        """getter for the private instance attribute height"""
         return self.__height
 
     @height.setter
     def height(self, value):
-        """Setter for the private instance"""
+        """setter for the private instance attribute height"""
         if type(value) is not int:
             raise TypeError("height must be an integer")
-        if type(value) < 0:
+        if value < 0:
             raise ValueError("height must be >= 0")
         self.__height = value
-
-
