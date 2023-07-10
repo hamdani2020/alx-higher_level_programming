a2d92d4927c4be020c3b775de5bf235b287d6ceb
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-07-10 15:39:45 -0700

    improve geometry

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+Contains the BaseGeometry class
+"""
+
+class BaseGeometry:
+    """publice attribute area class"""
+    def area(self):
+        """raises an exception when the the function is called"""
+        raise Exception("area() is not implemented")
