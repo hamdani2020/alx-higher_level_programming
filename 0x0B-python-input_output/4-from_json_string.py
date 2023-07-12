cea0a5ba1d77354a3721474323a450de08e6e82b
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-07-11 18:41:42 -0700

    From JSON string to Object

--- 0x0B-python-input_output/4-from_json_string.py
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 """
-It contains the json string function
+contains the json str function
 """
-
 
 import json
 
+
 def from_json_string(my_str):
-    """it returns an object represented by a JSON string"""
+    """returns an object represented by a JSON string"""
     return json.loads(my_str)
