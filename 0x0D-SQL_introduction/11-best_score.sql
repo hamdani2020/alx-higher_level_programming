810cb032133308395a33951391b9f033ee374021
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-08-16 04:01:50 -0700

    update

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,5 +1,5 @@
--- This script lists all the records with a score>= 10
-SELECT "score", "name"
-FROM "second_table"
-WHERE "score" >= 10
-ORDER BY "score" DESC;
+-- This script lists all the records with a score 
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `score` >= 10
+ORDER BY `score` DESC;
