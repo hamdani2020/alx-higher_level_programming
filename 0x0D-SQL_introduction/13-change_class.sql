c426723b7a899be558218342da2e39b22f24f1bd
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-08-16 04:13:43 -0700

    done with mandatory task

---
+++ 0x0D-SQL_introduction/13-change_class.sql
@@ -0,0 +1,3 @@
+-- This script removes all records with a score <= 5
+DELETE FROM `second_table`
+WHERE `score` <= 5;
---
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -0,0 +1,5 @@
+-- This script lists all records of the table
+SELECT `score`, `name`
+FROM `second_table`
+WHERE `name` != ""
+ORDER BY `score` DESC
