9dca694e4895e55a201589d688308c3a991c3c10
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-08-16 09:10:54 -0700

    advanced tasked

--- 0x0D-SQL_introduction/100-move_to_utf8.sql
+++ 0x0D-SQL_introduction/100-move_to_utf8.sql
@@ -1,4 +1,4 @@
 -- This script converts database to UTF8
 USE `hbtn_0c_0`
 ALTER TABLE `first_table`
-CONVERT TO CHARACTER SET utf8mb COLLATE utf8mb_unicode_ci;
+CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
