0b525e18b57f3273a832f12eb12ed287eb2bc898
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-08-17 11:21:52 -0700

    I am done for now, I will continue later

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,4 @@
+-- this script creates database
+CREATE DATABASE IF EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS user_0d_2@localhost IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO user_0d_2@localhost
---
+++ 0x0E-SQL_more_queries/4-never_empty.sql
@@ -0,0 +1,2 @@
+-- It creates a table
+CREATE TABLE IF EXISTS `id_not_null` (`id` INT	DEFAULT 1, `name` VARCHAR(256));
