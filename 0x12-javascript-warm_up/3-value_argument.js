bea8b0343c2af7e66ec4d8c58c6d4c48d885d494
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-09-11 18:29:16 -0700

    value arguments

--- 0x12-javascript-warm_up/3-value_argument.js
+++ 0x12-javascript-warm_up/3-value_argument.js
@@ -1,2 +1,2 @@
 #!/usr/bin/node
-console.log(typeof process.argv[3] === 'undefined' ? 'No argument' : process.argv[3]);
+console.log(typeof process.argv[2] === 'undefined' ? 'No argument' : process.argv[2]);
