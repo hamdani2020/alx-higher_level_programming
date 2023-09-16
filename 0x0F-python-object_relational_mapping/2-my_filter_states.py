fd5ac40c15dd798b98fd8eaed164b265cac07e88
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-09-16 14:02:09 -0700

    filter states by user inpurt

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+""" This script displays all values in the states table of the database """
+
+if __name__ == '__main__':
+
+    from sys import argv
+    import MySQLdb
+
+    db_user = argv[1]
+    db_password = argv[2]
+    db_name = argv[3]
+    search = argv[4]
+
+    database = MySQLdb.connect(host='localhost',
+                               port=3306,
+                               user = db_user,
+                               password = db_password,
+                               db=db_name)
+
+    cursor = database.cursor()
+
+    cursor.execute('SELECT id, name FROM states\
+                   WHERE states.name = \'{}\'\
+                   ORDER BY states.id ASC'.format(search))
+
+    for row in cursor.fetchall():
+        if row[1] == search:
+            print(row)
