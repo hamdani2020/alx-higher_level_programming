306f74f39598318747ab1918da3e579c86f71a69
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-09-29 15:28:53 -0700

    what's my status?

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""This script fetches response from a url"""
+
+import urllib.request
+
+if __name__ == "__main__":
+    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as response:
+        content = response.read()
+        print('Body response:')
+        print('\t- type: {}'.format(type(content)))
+        print('\t- content: {}'.format(type(content)))
+        print('\t- utf8 content: {}'.format(str(content)[2:-1]))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,8 @@
+# 0x11. Python - Network #1
+**Python**|**Scripting**|**Back-end**|**API**
+
+## Resources
+Read or watch
+- [HOWTO Fetch Internet Resources Using urllib Package](https://docs.python.org/3/howto/urllib2.html)
+- [Quickstart with Request package](https://requests.readthedocs.io/en/latest/)
+- [Requests package](https://pypi.org/project/requests/)
