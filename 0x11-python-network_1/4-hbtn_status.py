ade326fbd8121600cc45c9a25e7d430268cb4e75
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-09-29 16:16:18 -0700

    what's my status?

---
+++ 0x11-python-network_1/4-hbtn_status.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""This script fetches response from the web
+"""
+
+import requests
+
+if __name__ == '__main__':
+    
+    url = 'https://alx-intranet.hbtn.io/status'
+    response = requests.get(url)
+    print('Body response:')
+    print('\t- type: {}'.format(type(response.text)))
+    print('\t- content: {}'.format(response.text))
