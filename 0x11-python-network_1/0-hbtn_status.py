1c9f18f9e440c7d20ee5574642463e750d3b55d8
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-09-29 15:31:18 -0700

    what's my status?

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         content = response.read()
         print('Body response:')
         print('\t- type: {}'.format(type(content)))
-        print('\t- content: {}'.format(type(content)))
+        print('\t- content: {}'.format(content))
         print('\t- utf8 content: {}'.format(str(content)[2:-1]))
