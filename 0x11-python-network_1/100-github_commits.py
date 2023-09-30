46f6db4d9587db4545374bfaa78037eed54434f8
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-09-29 17:05:46 -0700

    Time for an interview

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""It requests commits via Github API"""
+
+from sys import argv
+import requests
+
+if __name__ == '__main__':
+
+    repo = argv[1]
+    owner = argv[2]
+
+    url = 'https://api.github.com/repos/{}/{}/\
+commits?per_page=10'.format(owner, repo)
+
+u = requests.get(url)
+u = u.json()
+
+for commit in u:
+    print('{}: {}'.format(commit.get('sha'), commit.get('commit').get('author').get('name')))
