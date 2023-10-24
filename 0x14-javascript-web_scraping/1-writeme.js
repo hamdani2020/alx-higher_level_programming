fcf5a246907649574ec7a62c21f9f9aec019a78a
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-10-24 04:40:30 -0700

    task 2

---
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const fs = require('fs');
+const filePath = process.argv[2];
+const dataWrite = process.argv[3];
+
+
+fs.writeFile(filePath, dataWrite, 'utf-8', (error) => {
+  if (error) {console.log(error); }
+});
---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,11 @@
+#!/usr/bin/node
+
+const request = require('require');
+const url = process.argv[2];
+
+
+request
+  .get(url)
+  .on('response', (response) => {
+	  console.log('code: ' + response.statusCode);
+  });
