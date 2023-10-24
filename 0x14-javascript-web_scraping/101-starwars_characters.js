25d489e058a56157e68ece55bdb26b0c1d1b67cc
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-10-24 09:40:22 -0700

    advanced task 2

--- 0x14-javascript-web_scraping/101-starwars_characters.js
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -1,22 +1,45 @@
 #!/usr/bin/node
 const request = require('request');
-const movieID = process.argv[2];
-const url = 'https://swapi-api.hbtn.io/api/films/' + movieID;
 
-request(url, async (error, response, body) => {
-  if (error) { console.log(error); }
-  const result = JSON.parse(body);
-
-  for (const charURL of result.characters) {
-    await new Promise((resolve, reject) => {
-      request(charURL, (err, r, body) => {
-        if (err) {
-          reject(err);
-        } else {
-          console.log(JSON.parse(body).name);
-          resolve();
-        }
-      });
+function getDataFrom (url) {
+  return new Promise(function (resolve, reject) {
+    request(url, function (err, _res, body) {
+      if (err) {
+        reject(err);
+      } else {
+        resolve(body);
+      }
     });
-  }
-});
+  });
+}
+
+function errHandler (err) {
+  console.log(err);
+}
+
+function printMovieCharacters (movieId) {
+  const movieUri = `https://swapi-api.hbtn.io/api/films/${movieId}`;
+
+  getDataFrom(movieUri)
+    .then(JSON.parse, errHandler)
+    .then(function (res) {
+      const characters = res.characters;
+      const promises = [];
+
+      for (let j = 0; j < characters.length; ++j) {
+        promises.push(getDataFrom(characters[j]));
+      }
+
+      Promise.all(promises)
+        .then((results) => {
+          for (let j = 0; j < results.length; ++j) {
+            console.log(JSON.parse(results[j]).name);
+          }
+        })
+        .catch((err) => {
+          console.log(err);
+        });
+    });
+}
+
+printMovieCharacters(process.argv[2]);
