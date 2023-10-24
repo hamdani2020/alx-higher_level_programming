0c1f115d73ee7c112748860f786cb677b410c919
hamdani2020 <hamdanialhassangandi2020@gmail.com>
2023-10-24 09:54:31 -0700

    hot fixed

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
+
 const fs = require('fs');
 const filePath = process.argv[2];
 const dataWrite = process.argv[3];
-
 
 fs.writeFile(filePath, dataWrite, 'utf-8', (error) => {
   if (error) {console.log(error); }
